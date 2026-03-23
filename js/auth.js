9963965b778fd78d0d6bb1a3f3dbc46bb1fdd84e
andrielemarquess <andriele.marques@academico.ifpb.edu.br>
2026-03-22 21:08:35 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,10 @@
+let usuarioLogado = false;
 
+export function fazerLogin(){
+    usuarioLogado = true;
+    alert("Login realizado!");
+}
+
+export function verificarLogin(){
+    return usuarioLogado;
+}
