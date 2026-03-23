82a2839422e7e15b557539a63b8228a148d2cf30
andrielemarquess <andriele.marques@academico.ifpb.edu.br>
2026-03-22 21:08:14 -0300

    Update navegacao.js

--- js/navegacao.js
+++ js/navegacao.js
@@ -1,1 +1,17 @@
+export function mostrarPagina(pagina){
+    const paginas = ['home','registro','login','publicar'];
 
+    paginas.forEach(p => {
+        const el = document.getElementById(p);
+        if(el){
+            el.style.opacity = 0;
+            setTimeout(() => el.style.display = 'none', 500);
+        }
+    });
+
+    setTimeout(() => {
+        const mostrar = document.getElementById(pagina);
+        mostrar.style.display = (pagina === 'home') ? 'grid' : 'block';
+        setTimeout(() => mostrar.style.opacity = 1, 50);
+    }, 500);
+}
