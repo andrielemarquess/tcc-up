25613974460bc0a34bd502663bde1fb386540b17
andrielemarquess <andriele.marques@academico.ifpb.edu.br>
2026-03-22 21:08:59 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,13 @@
+export function abrirDetalhes(titulo,descricao,autor,orientador,data,instituicao){
+    document.getElementById("titulo").innerText = titulo;
+    document.getElementById("descricao").innerText = descricao;
+    document.getElementById("autor").innerText = autor;
+    document.getElementById("orientador").innerText = orientador;
+    document.getElementById("data").innerText = data;
+    document.getElementById("instituicao").innerText = instituicao;
+    document.getElementById("modal").style.display = "flex";
+}
 
+export function fecharModal(){
+    document.getElementById("modal").style.display = "none";
+}
